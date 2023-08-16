719a40112179bb9957e4dc99ad0dbc405269d773
Anand <jeevaanand93002@gmail.com>
2023-08-16 12:05:58 +0530

    youtube clone

---
+++ app/src/main/java/com/example/mediaplayer/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.mediaplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.media3.common.MediaItem;
+import androidx.media3.exoplayer.ExoPlayer;
+import androidx.media3.ui.PlayerView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    PlayerView playView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        playView = findViewById(R.id.plView);
+        ExoPlayer exoPlayer = new ExoPlayer.Builder(this).build();
+        MediaItem md = MediaItem.fromUri("https://storage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4");
+        exoPlayer.setMediaItem(md);
+        playView.setPlayer(exoPlayer);
+
+        exoPlayer.prepare();
+
+    }
+}
